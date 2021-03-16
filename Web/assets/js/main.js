5bd3d76dc34259ee5656e1e46d53f4263e08d70e
LAGA <gutierrezanicamaluisalberto@hotmail.com>
2021-03-15 22:10:42 -0500

    Retoques a algunos archivos

--- Web/assets/js/main.js
+++ Web/assets/js/main.js
@@ -1,7 +1,7 @@
 
 (function ($) {
   "use strict";
-  // TOP Menu Sticky
+  
   $(window).on('scroll', function () {
     var scroll = $(window).scrollTop();
     if (scroll < 400) {
@@ -13,10 +13,7 @@
     }
   });
 
-
   $(document).ready(function () {
-
-    // mobile_menu
     var menu = $('ul#navigation');
     if (menu.length) {
       menu.slicknav({
@@ -26,7 +23,6 @@
       });
     };
 
-    // review-active
     $('.slider_active').owlCarousel({
       loop: true,
       margin: 0,
@@ -37,18 +33,12 @@
       autoplayHoverPause: true,
       autoplaySpeed: 800,
       responsive: {
-        0: {
-          items: 1
-        },
-        767: {
-          items: 1
-        },
-        992: {
-          items: 1
-        }
+        0: {items: 1},
+        767: {items: 1},
+        992: {items: 1}
       }
     });
-    // review-active
+
     $('.textmonial_active').owlCarousel({
       loop: true,
       margin: 100,
@@ -60,21 +50,12 @@
       autoplayHoverPause: true,
       autoplaySpeed: 800,
       responsive: {
-        0: {
-          items: 1,
-          nav: false,
-        },
-        767: {
-          items: 1,
-          nav: true,
-        },
-        992: {
-          items: 1
-        }
+        0: {items: 1, nav: false,},
+        767: {items: 1, nav: true,},
+        992: {items: 1}
       }
     });
 
-    // about_active
     $('.about_active').owlCarousel({
       loop: true,
       margin: 0,
@@ -86,21 +67,12 @@
       autoplayHoverPause: true,
       autoplaySpeed: 800,
       responsive: {
-        0: {
-          items: 1,
-          nav: false,
-        },
-        767: {
-          items: 1,
-          nav: false,
-        },
-        992: {
-          items: 1
-        }
+        0: {items: 1, nav: false,},
+        767: {items: 1, nav: false,},
+        992: {items: 1}
       }
     });
 
-    // review-active
     $('.testmonial_active').owlCarousel({
       loop: true,
       margin: 0,
@@ -112,27 +84,11 @@
       autoplayHoverPause: true,
       autoplaySpeed: 800,
       responsive: {
-        0: {
-          items: 1,
-          dots: false,
-          nav: false,
-        },
-        767: {
-          items: 1,
-          dots: false,
-          nav: false,
-        },
-        992: {
-          items: 1,
-          nav: false
-        },
-        1200: {
-          items: 1,
-          nav: false
-        },
-        1500: {
-          items: 1
-        }
+        0: {items: 1, dots: false, nav: false,},
+        767: {items: 1, dots: false, nav: false,},
+        992: {items: 1, nav: false},
+        1200: {items: 1, nav: false},
+        1500: {items: 1}
       }
     });
 
@@ -146,51 +102,25 @@
       autoplayHoverPause: true,
       autoplaySpeed: 800,
       responsive: {
-        0: {
-          items: 1,
-          nav: false
-
-        },
-        767: {
-          items: 4
-        },
-        992: {
-          items: 7
-        }
+        0: {items: 1, nav: false},
+        767: {items: 4},
+        992: {items: 7}
       }
     });
 
-    //project-active
     $('.project-active').owlCarousel({
       loop: true,
       margin: 30,
       items: 1,
-      // autoplay:true,
       navText: ['<i class="Flaticon flaticon-left-arrow"></i>', '<i class="Flaticon flaticon-right-arrow"></i>'],
       nav: true,
       dots: false,
-      // autoplayHoverPause: true,
-      // autoplaySpeed: 800,
       responsive: {
-        0: {
-          items: 1,
-          nav: false
-
-        },
-        767: {
-          items: 1,
-          nav: false
-        },
-        992: {
-          items: 2,
-          nav: false
-        },
-        1200: {
-          items: 1,
-        },
-        1501: {
-          items: 2,
-        }
+        0: {items: 1, nav: false},
+        767: {items: 1, nav: false},
+        992: {items: 2, nav: false},
+        1200: {items: 1,},
+        1501: {items: 2,}
       }
     });
 
@@ -198,37 +128,19 @@
       $('select').niceSelect();
     }
 
-    //about-pro-active
     $('.details_active').owlCarousel({
       loop: true,
       margin: 0,
       items: 1,
-      // autoplay:true,
       navText: ['<i class="ti-angle-left"></i>', '<i class="ti-angle-right"></i>'],
       nav: true,
       dots: false,
-      // autoplayHoverPause: true,
-      // autoplaySpeed: 800,
       responsive: {
-        0: {
-          items: 1,
-          nav: false
-
-        },
-        767: {
-          items: 1,
-          nav: false
-        },
-        992: {
-          items: 1,
-          nav: false
-        },
-        1200: {
-          items: 1,
-        }
+        0: {items: 1, nav: false},
+        767: {items: 1, nav: false},
+        992: {items: 1, nav: false},
+        1200: {items: 1,}
       }
     });
-
   });
-
 })(jQuery);
