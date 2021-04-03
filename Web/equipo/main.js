3653fa7a7bd96b9758254b8ff0214d2ee68bdc0b
LAGA <gutierrezanicamaluisalberto@hotmail.com>
2021-04-03 00:42:41 -0500

    Revisar equipo

---
+++ Web/equipo/main.js
@@ -0,0 +1,30 @@
+
+(function ($) {
+  "use strict";
+  
+  // $(window).on('scroll', function () {
+  //   var scroll = $(window).scrollTop();
+  //   if (scroll < 400) {
+  //     $("#sticky-header").removeClass("sticky");
+  //     $('#back-top').fadeIn(500);
+  //   } else {
+  //     $("#sticky-header").addClass("sticky");
+  //     $('#back-top').fadeIn(500);
+  //   }
+  // });
+
+  $(document).ready(function () {
+    var menu = $('ul#navigation');
+    if (menu.length) {
+      menu.slicknav({
+        prependTo: ".mobile_menu",
+        closedSymbol: '+',
+        openedSymbol: '-'
+      });
+    };
+    if (document.getElementById('default-select')) {
+      $('select').niceSelect();
+    }
+
+  });
+})(jQuery);
