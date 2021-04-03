6b25b738a7a13a9d07cad9bb6e5628aa1a418a0a
LAGA <gutierrezanicamaluisalberto@hotmail.com>
2021-04-03 11:07:40 -0500

    Página de nuestro equipo OK

--- Web/equipo/main.js
+++ Web/equipo/main.js
@@ -2,16 +2,16 @@
 (function ($) {
   "use strict";
   
-  // $(window).on('scroll', function () {
-  //   var scroll = $(window).scrollTop();
-  //   if (scroll < 400) {
-  //     $("#sticky-header").removeClass("sticky");
-  //     $('#back-top').fadeIn(500);
-  //   } else {
-  //     $("#sticky-header").addClass("sticky");
-  //     $('#back-top').fadeIn(500);
-  //   }
-  // });
+  $(window).on('scroll', function () {
+    var scroll = $(window).scrollTop();
+    if (scroll < 400) {
+      $("#sticky-header").removeClass("sticky");
+      $('#back-top').fadeIn(500);
+    } else {
+      $("#sticky-header").addClass("sticky");
+      $('#back-top').fadeIn(500);
+    }
+  });
 
   $(document).ready(function () {
     var menu = $('ul#navigation');
